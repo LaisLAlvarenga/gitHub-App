5b3aa21d798d03712b74553315831bc428f52ab4
Lais Luderer Alvarenga <lais.alvarenga.global@gmail.com>
2023-01-16 15:49:29 -0300

    Project completed

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
-//const url = 'https://api.github.com/users';
-const urlFollowers = 'https://api.github.com/users/giovannamoeller/followers';
+const inputname = document.querySelector('#inputSearch');
+const buttonInputName = document.querySelector('#searchButton');
+const buttonCleanUser = document.querySelector('#cleanButton');
 
+const users = document.querySelector('.users');
 const followers = document.querySelector('.followers');
 
+const buttonSearch = document.querySelector('#button-addon2'); /* fILTER */
+
 async function getData() {
-    const response = await fetch(urlFollowers).then(response => response.json());    
-    console.log(response);
+    const inputName = inputname.value;
+    const response = await fetch(`https://api.github.com/users/${inputName}/followers`).then(response => response.json());    
     
     return response;
+}
+
+async function getUserMain() {
+    const inputName = inputname.value;
+    await fetch(`https://api.github.com/users/${inputName}`).then(response => response.json()).then(data => {
+        const divUser = document.createElement('div');
+        divUser.setAttribute('class', 'user');
+
+        const avatarMain = document.createElement('img');
+        avatarMain.setAttribute('class', 'avatarImg');
+        avatarMain.src = data.avatar_url;
+
+        const nameMain = document.createElement('p');
+        nameMain.setAttribute('class', 'name');
+        nameMain.textContent = data.name;
+        
+        const loginMain = document.createElement('p');
+        loginMain.setAttribute('class', 'login');
+        loginMain.textContent = data.login;
+
+        const bioMain = document.createElement('p');
+        bioMain.setAttribute('class', 'bio');
+        bioMain.textContent = data.bio;
+
+        divUser.appendChild(avatarMain)
+        divUser.appendChild(nameMain)
+        divUser.appendChild(loginMain)
+        divUser.appendChild(bioMain)
+
+        users.appendChild(divUser);
+    });
+
+    const data = await getData();
+    data.forEach( user => renderUsers(user) );
 }
 
 function renderUsers(user) {
@@ -15,6 +53,7 @@ function renderUsers(user) {
     follower.setAttribute('class', 'follower');
 
     const avatar = document.createElement('img');
+    avatar.setAttribute('class', 'avatarImgUser');
     avatar.src = user.avatar_url;
 
     const loginUser = document.createElement('p');
@@ -27,12 +66,27 @@ function renderUsers(user) {
     followers.appendChild(follower);
 }
 
-window.onload = async function() {
-    const data = await getData();
-    const urlfollowers = data.followers_url;
-    console.log(urlfollowers)
+async function filter() {
+    const inputSearch = document.querySelector('#inputFilter').value;
 
-    data.forEach(user => {
-        renderUsers(user)
-    });
+    const users = await getData();
+    const resultFilter = users.filter(word => word.login == inputSearch);
+
+    if(resultFilter.length != 0) {
+        resultFilter.forEach(test => {
+            followers.innerHTML = '';
+            renderUsers(test)
+        });
+    }
+    else {
+        followers.innerHTML = '<p>Nenhum resultado encontrado.</p>'
+    }    
 }
+
+buttonInputName.addEventListener('click', getUserMain);
+
+buttonSearch.addEventListener('click', filter);
+
+buttonCleanUser.addEventListener('click', function(){
+    location.reload();
+})
